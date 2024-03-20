65fedd5c1225f7ce7561e7c66611a466e85094c5
pjanosky <pcjanosky@gmail.com>
2024-03-19 22:32:23 -0400

    assignment 5

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
